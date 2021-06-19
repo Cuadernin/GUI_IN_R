fe2f58ab25886be5c3f0420e94c993a701d4779b
Erwin <75356078+Cuadernin@users.noreply.github.com>
2021-06-18 20:59:16 -0500

    Binomial model for options

---
+++ Binomial.R
@@ -0,0 +1,29 @@
+bino2=function(S,K,T,r,n,o,put=FALSE,am=FALSE){
+  t=T/n
+  u=exp(o*sqrt(t))
+  d=1/u
+  p=(exp(r*t)-d)/(u-d)
+  val=matrix(0,n+1,n+1);C=matrix(0,n+1,n+1)
+  pos=ifelse(put,-1,1)
+  for(i in 1:n+1){
+    for(j in 1:i){
+      st=S*(u^(j-1)*d^(i-j))
+      val[i,j]=st
+    }
+  }
+  C[nrow(C),]=pmax(pos*(val[nrow(val),]-K),0)
+  for(i in (nrow(C)-1):1){
+    for(j in 1:i){
+      if(am){
+        v1=exp(-r*t)*(p*C[i+1,j+1]+(1-p)*C[i+1,j])
+        st=pos*(S*u^(j-1)*d^(i-j)-K)
+        v2=max(st,0)
+        C[i,j]=max(v1,v2)
+      }
+      else{
+        C[i,j]=exp(-r*t)*(p*C[i+1,j+1]+(1-p)*C[i+1,j])
+      }
+    }
+  }
+  return(C[1,1])
+}
